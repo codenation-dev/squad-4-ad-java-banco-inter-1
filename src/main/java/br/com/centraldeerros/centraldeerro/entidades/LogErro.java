78d79ec175566de5731ada1873138328307bdf56
mayconbj15 <mayconbj15@gmail.com>
2019-12-01 22:37:32 -0300

    Terminando mapeamento de entidades

--- src/main/java/br/com/centraldeerros/centraldeerro/entidades/LogErro.java
+++ src/main/java/br/com/centraldeerros/centraldeerro/entidades/LogErro.java
@@ -1,25 +1,48 @@
 package br.com.centraldeerros.centraldeerro.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+@Entity
+@Table
 public class LogErro {
 
+    @Id
     private Long id;
 
+    @Column
+    @NotNull
+    @NotBlank
     private String titulo;
 
+    @Column
     private String detalhes;
 
+    @Column
+    @NotNull
+    @NotBlank
     private String origem;
 
+    @Column
+    @NotNull
+    @NotBlank
     private LocalDateTime dataOcorrencia;
 
+    @Column
     private LocalDateTime dataEnvioOcorrencia;
 
     // revisar esse atributo
+    @Column
     private String token;
 
+    @Column
     private String plataforma;
 
+    @Column
     private String versaoPlataforma;
 }
