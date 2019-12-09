d9ee1cff2db1f2ec51cf825d0d85c5ac08b1bcff
lucasmoreiradeoliveira <lucas.oliveira3001@gmail.com>
2019-12-09 07:19:17 -0300

    Adicionado Swagger

--- src/main/java/br/com/centraldeerros/centraldeerro/CentralDeErroApplication.java
+++ src/main/java/br/com/centraldeerros/centraldeerro/CentralDeErroApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication(scanBasePackages = {"br.com.centraldeerros.centraldeerro"})
 @EnableJpaRepositories(basePackages = {"br.com.centraldeerros.centraldeerro.repositories"})
+//@EnableSpringDataWebSupport
 public class CentralDeErroApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/centraldeerros/centraldeerro/Configurations/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package br.com.centraldeerros.centraldeerro.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDoc() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("br.com.centraldeerros.centraldeerro.controllers"))
+                    .paths(PathSelectors.ant("/**"))
+                    .build()
+                .apiInfo(metaData())
+                .globalOperationParameters(Arrays.asList( //ADICIONA PARAMETROS GLOBAIS EM TODOS OS ENDPOINTS
+                        new ParameterBuilder()
+                                .name("Authorization") //NOME DO PARAMETRO
+                                .description("Header para token JWT") //DESCRICAO
+                                .modelRef(new ModelRef("string")) //O TIPO DELE
+                                .parameterType("header") //ONDE ELE SERÁ ENVIADO
+                                .required(false) //SE É NECESSÁRIO
+                                .build()));
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Codenation Central de Erros")
+                .description("The best spring course out there")
+                .version("1.0")
+                .contact(new Contact("Lucas/Marcos/Maycon", "url", "email@email.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/license/LICENSE-2.0")
+                .build();
+    }
+
+}
